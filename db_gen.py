bcc2b7d73daceb3ede6b071b44a95a60d70dfa95
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-12-08 13:35:41 +0800

    Update db_gen.py

--- db_gen.py
+++ db_gen.py
@@ -14,8 +14,13 @@ Dataset_Name = 'Dataset_Zhitang_Yolo5'
 Classes = {'bleeding': 0, 'microaneurism': 1, 'soft_exudate': 2, 'hard_exudate': 3, 'IRMA': 4, 'laser_spot': 5,
            'new_blood_vessels': 6, 'optic_disk': 7, 'macular': 8}
 
-is_display = False
+is_display = True
 
+
+'''Cut a part of the input image'''
+def image_roi(img, x1, y1, x2, y2):
+    img = img[x1:x2, y1:y2]
+    return img
 
 main_db = pd.read_csv('ZhitangSeg1K.csv', keep_default_na=False)
 ''' Read the CSV file '''
@@ -95,6 +100,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(bld_mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -125,12 +132,6 @@ for i in range(len(main_db['image_url'])):
             if is_display:
                 orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 0),
                                 thickness=2)
-            # plt.imshow(orig)
-            # plt.show()
-            # plt.imshow(resized)
-            # plt.show()
-            # print('okay')
-        # input()
 
     ''' Check the Microaneurism mask URL exist '''
     if microaneurism_url not in (None, ""):
@@ -162,6 +163,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -219,6 +222,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -243,7 +248,7 @@ for i in range(len(main_db['image_url'])):
                     bl_h_n) + '\n')
 
             if is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 255),
+                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(0, 0, 255),
                                 thickness=2)
 
     ''' Check the hard_exudate mask URL exist '''
@@ -276,6 +281,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -300,7 +307,7 @@ for i in range(len(main_db['image_url'])):
                     bl_h_n) + '\n')
 
             if is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 255),
+                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 255, 0),
                                 thickness=2)
 
     ''' Check the irma mask URL exist '''
@@ -333,6 +340,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -389,6 +398,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -413,7 +424,7 @@ for i in range(len(main_db['image_url'])):
                     bl_h_n) + '\n')
 
             if is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 255),
+                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(0, 255, 255),
                                 thickness=2)
     ''' Check the new blood vessel mask URL exist '''
     if nbv_url not in (None, ""):
@@ -445,6 +456,8 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv.boundingRect(cnt)
@@ -469,7 +482,7 @@ for i in range(len(main_db['image_url'])):
                     bl_h_n) + '\n')
 
             if is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 255),
+                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 255, 255),
                                 thickness=2)
     ''' Check the optic dist mask URL exist '''
     if optic_disk_url not in (None, ""):
@@ -501,32 +514,36 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            x, y, w, h = cv.boundingRect(cnt)
-            x_box = x_orig + x
-            y_box = y_orig + y
 
-            '''Convert X, Y , Width, Height to the new Scale'''
-            bl_x = x_box * ar_width
-            bl_y = y_box * ar_height
-            bl_w = w * ar_width
-            bl_h = h * ar_height
+        max_cnt = max(contours, key=cv.contourArea)
+        # for cnt in contours:
+        x, y, w, h = cv.boundingRect(max_cnt)
+        x_box = x_orig + x
+        y_box = y_orig + y
 
-            '''Normalize the results'''
-            bl_x_n = bl_x / Image_Dim[0]
-            bl_y_n = bl_y / Image_Dim[1]
-            bl_w_n = bl_w / Image_Dim[0]
-            bl_h_n = bl_h / Image_Dim[1]
+        '''Convert X, Y , Width, Height to the new Scale'''
+        bl_x = x_box * ar_width
+        bl_y = y_box * ar_height
+        bl_w = w * ar_width
+        bl_h = h * ar_height
 
-            ''' Write the bounding box into the file '''
-            labels_file.write(
-                str(Classes['optic_disk']) + ' ' + str(bl_x_n) + ' ' + str(bl_y_n) + ' ' + str(bl_w_n) + ' ' + str(
-                    bl_h_n) + '\n')
+        '''Normalize the results'''
+        bl_x_n = bl_x / Image_Dim[0]
+        bl_y_n = bl_y / Image_Dim[1]
+        bl_w_n = bl_w / Image_Dim[0]
+        bl_h_n = bl_h / Image_Dim[1]
 
-            if not is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 100, 255),
-                                thickness=2)
+        ''' Write the bounding box into the file '''
+        labels_file.write(
+            str(Classes['optic_disk']) + ' ' + str(bl_x_n) + ' ' + str(bl_y_n) + ' ' + str(bl_w_n) + ' ' + str(
+                bl_h_n) + '\n')
+
+        if is_display:
+            orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 100, 255),
+                            thickness=2)
 
     ''' Check the macular mask URL exist '''
     if macular_url not in (None, ""):
@@ -558,37 +575,41 @@ for i in range(len(main_db['image_url'])):
         '''Find Contours (find all the bleeding parts in the mask image)'''
         bld_gray = cv.cvtColor(mask, cv.COLOR_RGB2GRAY)
         ret, bld_thresh = cv.threshold(bld_gray, 10, 255, cv.THRESH_BINARY)
+        kernel = np.ones((5, 5), np.uint8)
+        bld_thresh = cv.morphologyEx(bld_thresh, cv.MORPH_CLOSE, kernel)
         contours, hierarchy = cv.findContours(bld_thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            x, y, w, h = cv.boundingRect(cnt)
-            x_box = x_orig + x
-            y_box = y_orig + y
+        max_cnt = max(contours, key=cv.contourArea)
+        # for cnt in contours:
+        x, y, w, h = cv.boundingRect(max_cnt)
+        x_box = x_orig + x
+        y_box = y_orig + y
 
-            '''Convert X, Y , Width, Height to the new Scale'''
-            bl_x = x_box * ar_width
-            bl_y = y_box * ar_height
-            bl_w = w * ar_width
-            bl_h = h * ar_height
+        '''Convert X, Y , Width, Height to the new Scale'''
+        bl_x = x_box * ar_width
+        bl_y = y_box * ar_height
+        bl_w = w * ar_width
+        bl_h = h * ar_height
 
-            '''Normalize the results'''
-            bl_x_n = bl_x / Image_Dim[0]
-            bl_y_n = bl_y / Image_Dim[1]
-            bl_w_n = bl_w / Image_Dim[0]
-            bl_h_n = bl_h / Image_Dim[1]
+        '''Normalize the results'''
+        bl_x_n = bl_x / Image_Dim[0]
+        bl_y_n = bl_y / Image_Dim[1]
+        bl_w_n = bl_w / Image_Dim[0]
+        bl_h_n = bl_h / Image_Dim[1]
 
-            ''' Write the bounding box into the file '''
-            labels_file.write(
-                str(Classes['macular']) + ' ' + str(bl_x_n) + ' ' + str(bl_y_n) + ' ' + str(bl_w_n) + ' ' + str(
-                    bl_h_n) + '\n')
+        ''' Write the bounding box into the file '''
+        labels_file.write(
+            str(Classes['macular']) + ' ' + str(bl_x_n) + ' ' + str(bl_y_n) + ' ' + str(bl_w_n) + ' ' + str(
+                bl_h_n) + '\n')
 
-            if is_display:
-                orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 0, 255),
-                                thickness=2)
+        if is_display:
+            orig = cv.rectangle(orig, rec=(int(x_box), int(y_box), int(w), int(h)), color=(255, 255, 100),
+                            thickness=2)
 
+    orig = cv.cvtColor(orig, cv.COLOR_RGB2BGR)
     plt.imshow(orig)
     plt.show()
     labels_file.close()
-    input()
+    # input()
     #     limg = cv.imread(os.path.join(out_db_dir, out_img_dir, filename))
     #     ''' Read the downloaded fundus image using OpenCV API '''
     #     
