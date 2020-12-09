f227759f242ba1988901b44e35df7d7ba26cd112
Ahmad Karambakhsh <ahmad.karambakhsh@gmail.com>
2020-12-09 16:55:46 +0800

    Create train_test_val.py

---
+++ train_test_val.py
@@ -0,0 +1,10 @@
+import os
+
+input_dir = '.'
+output_dir = '.'
+
+in_img_dir = os.path.join(input_dir, 'Images')
+in_label_dir = os.path.join(input_dir, 'Labels')
+out_img_dir = os.path.join(output_dir, 'images')
+out_label_dir = os.path.join(output_dir, 'labels')
+
